ee9730ea923f80455eafbc1870f9f2ff77ca9e0a
ILIA PETROV <ilia-p@yandex.ru>
2017-05-20 15:30:43 +0300

    Код ф-ии для нахождения корней оптимизирован

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -2,15 +2,13 @@ from math import sqrt
 
 
 def get_roots(a, b, c):
-    discriminant = b ** 2 - 4 * a * c
-    try:
-    	root1 = (-b - sqrt(discriminant)) / (2 * a)
-    	root2 = (-b + sqrt(discriminant)) / (2 * a)
-    except ValueError:
-    	pass
-    if discriminant == 0:
-        return root1, None
-    if discriminant < 0:
-        return None, None
-    else:
-        return root1, root2
+	discriminant = b ** 2 - 4 * a * c
+	if discriminant >= 0:
+		root1 = (-b - sqrt(discriminant)) / (2 * a)
+		root2 = (-b + sqrt(discriminant)) / (2 * a)
+		if discriminant == 0:
+			return root1, None
+		else:
+			return root1, root2
+	else:
+		return None, None
